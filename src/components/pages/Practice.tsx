628aed69b7d722e0a357da46968a73fa9a5a2b7d
Jan Kratochvil <jan@doist.com>
2021-09-12 18:19:22 +0200

    Removes unneeded import

--- src/components/pages/Practice.tsx
+++ src/components/pages/Practice.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Redirect, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { shuffleArray } from "../../functions/array";
 import {
     getInstruction,
