1a27419fdc6fded8519001aa4715df4fa0722003
Jan Kratochvil <jan@doist.com>
2021-09-12 21:50:06 +0200

    Adds buy me a coffee button

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -2,9 +2,17 @@ import "./Footer.scss";
 
 const Footer = () => (
     <div className="footer-root">
-        <div>
-            by <a href="https://janjan.xyz">Jan</a>
-        </div>
+        <a
+            href="https://www.buymeacoffee.com/janjanxyz"
+            target="_blank"
+            rel="noreferrer"
+        >
+            <img
+                src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+                alt="Buy Me A Coffee"
+                className="buymeacoffee"
+            />
+        </a>
         <div>
             attributions and source on{" "}
             <a href="https://github.com/jankratochvilcz/kiriru/">GitHub</a>
